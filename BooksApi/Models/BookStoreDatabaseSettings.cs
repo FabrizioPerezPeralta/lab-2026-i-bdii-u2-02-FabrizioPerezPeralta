2333aa9bc83c4687c0bb4f52629e4367b18801e5
FabrizioPerezPeralta <fp2023077476@virtual.upt.pe>
2026-06-15 02:58:44 -0500

    Creación del API

---
+++ BooksApi/Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,9 @@
+namespace BooksApi.Models
+{
+    public class BookStoreDatabaseSettings
+    {
+        public string BooksCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
